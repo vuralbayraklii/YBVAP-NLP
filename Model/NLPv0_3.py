7e4d56505e86e4e329c0662b5c4f76ced4cb3acf
vuralbayraklii <vural.bayrakli@mrc-tr.com>
2025-11-04 12:06:48 +0300

    g

--- Model/NLPv0_3.py
+++ Model/NLPv0_3.py
@@ -997,6 +997,5 @@ if __name__ == "__main__":
 
     # Excel olarak kaydet (opsiyonel)
     final_df.to_excel(output_path, index=False, engine='openpyxl')
-    print()
     print(f"💾 Sonuçlar '{output_path}' dosyasına kaydedildi.")
 
