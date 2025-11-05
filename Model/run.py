34aa2a52b6316096fdf0cd2c77ba4899ea2a668c
vuralbayraklii <vural.bayrakli@mrc-tr.com>
2025-11-05 13:16:46 +0300

    Config mimarisi değiştirildi.

--- Model/run.py
+++ Model/run.py
@@ -3,14 +3,14 @@ from ZemberekClient import ZemberekClient
 from Fonksiyonlar import *
 import pandas as pd
 from idBasedCategoryMatcher import IDBasedCategoryMatcher
-from config import NLPConfig
+from config import NLPConfig, PathType
 from arıza_işleme import *
 import random
 from tqdm import tqdm
 from datetime import datetime
 from process import process_all_data
 from NLPv0_3 import ZemberekClientIface, BeamSearchCorrector, Weights, Thresholds, STOP_WORDS
-
+import json
 
 # -----------------------------
 # Değişkenler
@@ -20,13 +20,9 @@ config_path = r"C:\Users\vural.bayrakli\OneDrive - MRC\İletişim sitesi - MRC20
 
 config = NLPConfig.from_json(config_path, "vural.bayrakli")
 
-print(config.host)
-print(config.port)
-print(config.dokunulmayacak_kelimeler_path)
-print(config.arızalar_path)
-print(config.input_concatted_path)
-print(config.çözüm_açıklama_to_cause_code_path)
-
+# config dosyası oku
+with open(config_path, 'r', encoding='utf-8') as f:
+    config_data = json.load(f)
 
 output_dir = r"C:\Users\vural.bayrakli\OneDrive - MRC\İletişim sitesi - MRC2024-104-ADM-GDZ - Yerli Buyuk Veri Analitigi Platformu\05_Proje Çalışmaları\Vural\TextMiningwithGit\YBVAP-NLP\output"
 os.makedirs(output_dir, exist_ok=True)
@@ -39,15 +35,30 @@ if __name__ == "__main__":
     zemb = ZemberekClient(host=config.host, port=config.port)
     zemb_cli = ZemberekClientIface(zemb)
     
-    dokunma = pd.read_excel(config.dokunulmayacak_kelimeler_path)["kelimeler"].values
+    dokunma = pd.read_excel(config.get_path('dokunulmayacak_kelimeler_path'))["kelimeler"].values
 
-    df = pd.read_excel(config.arızalar_path)
-    
-    çözüm_açıklama_to_cause_code = pd.read_excel(config.çözüm_açıklama_to_cause_code_path)    
-    
-    çözüm_açıklama_info = pd.read_excel(config.çözüm_açıklama_info_path, sheet_name="Çözüm_Açıklama_Şebeke_Unsuru")
-    cause_code_info = pd.read_excel(config.çözüm_açıklama_info_path, sheet_name="cause_code_Şebeke_Unsuru")
-    
+    PATH_GÜNCELLEME = config_data["NLP"]["Path_Güncelleme"]
+    # Arızalar verisini güncelle
+    config.add_path_template(
+        'arızalar_path',
+    '{base}/{ana}/' + PATH_GÜNCELLEME["arızalar"]["path"],
+        path_type=PathType.FILE
+    )
+
+    df = pd.read_excel(config.get_path('arızalar_path'), sheet_name=PATH_GÜNCELLEME["arızalar"]["sheet_name"])
+
+    çözüm_açıklama_to_cause_code = pd.read_excel(config.get_path('çözüm_açıklama_to_cause_code_path'))
+
+    # Çözüm Açıklama Şebeke Unsuru verisi yolu güncellemesi
+    config.add_path_template(
+        'çözüm_açıklama_info_path',
+        '{base}/{ana}/' + PATH_GÜNCELLEME["çözüm_açıklama_info"]["path"],
+        path_type=PathType.FILE
+    )
+
+    çözüm_açıklama_info = pd.read_excel(config.get_path('çözüm_açıklama_info_path'), sheet_name= PATH_GÜNCELLEME["çözüm_açıklama_info"]["sheet_name"])
+    cause_code_info = pd.read_excel(config.get_path('çözüm_açıklama_info_path'), sheet_name= PATH_GÜNCELLEME["cause_code_info"]["sheet_name"])
+
     structures = build_morphosemantic_structures_v3(
         df,
         zemb,
@@ -59,12 +70,6 @@ if __name__ == "__main__":
     )
 
     _input = pd.read_excel(config.input_concatted_path)
-    
-    sayilar = random.choices(range(1, len(_input)), k=250)
-    sayilar = sorted(sayilar)
-    
-    input_sample = _input.iloc[sayilar]
-    input_sample.reset_index(drop=True, inplace = True)
     
     # Corrector'ı oluştur
     corrector = BeamSearchCorrector(
@@ -80,23 +85,9 @@ if __name__ == "__main__":
             min_token_len_for_split=4
         )
     )
-        
-    self = BeamSearchCorrector(
-        zemb_cli, 
-        dokunma,
-        structures.lemma2id,
-        weights=Weights(),
-        thresholds=Thresholds(
-            beam_width=10,
-            tau_replace=0.30,
-            tau_split=0.20,
-            tau_merge=0.25,
-            min_token_len_for_split=4
-        ))
     
     matcher = IDBasedCategoryMatcher(df, zemb, structures)
-    self = matcher
-        
+
     final_df = process_all_data(
         _input=_input,
         zemb=zemb,
